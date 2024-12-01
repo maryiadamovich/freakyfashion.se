d1aa23210e8b76e7c9e7f90fb98ad874510a4a5b
Maryia Adamovich <marika.tr77@gmail.com>
2024-12-01 14:18:47 +0100

    Clear database, update table

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,8 +11,16 @@ function fetchDataGet() {
     fetch("/admin/products/api/products")
         .then(resp => resp.json())
         .then(products => {
+            const table = document.querySelector("table");
             const emptyElem = document.querySelector(".empty");
-            emptyElem.parentNode.removeChild(emptyElem);
+            if (emptyElem) {
+                emptyElem.remove();
+            } else {
+                const rows = table.querySelectorAll('tr');
+                for (let i = 1; i < rows.length; i++) {
+                    rows[i].remove();
+                }
+            }
             products.forEach(element => {
                 const tr = document.createElement("tr");
                 for (let x in element) {
